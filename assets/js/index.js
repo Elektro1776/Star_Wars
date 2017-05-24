2d63d81cfe23461c25924fdb68564760538ec775
Austin <elektricwebdesign@gmail.com>
2017-05-24 10:43:37 -0600

    Added a bunch of possible assets for the project

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,37 @@
 $(document).ready(function() {
-  console.log(' DOCUMENT IS READY!!!!');
-})
+
+    const characters = {
+      yoda: '',
+      luke: '',
+      obiWan: '',
+      darthVader: '',
+      darthMaul: '',
+      darthSidious: '',
+    };
+
+    let playerCharacter,
+        enemies;
+
+    const jediStartLogo = $('#jediStartLogo');
+    const empireStartLogo = $('#empireStartLogo');
+    const init = () => {
+      const that = {};
+      function slide(el, top,left,right) {
+        el.animate({
+          top: top,
+          left: left,
+          right: right,
+          opacity: '1',
+        }, 1000)
+      }
+      that.startGame = () => {
+      
+        slide(jediStartLogo, '+=100px', '+=500px', '');
+        slide(empireStartLogo, '+=100px', '','-=500px');
+      }
+      return that;
+    };
+    var game = init();
+    game.startGame();
+    console.log(' WHAT ARE THE LOGO ELSSS', jediStartLogo, "Im the empire", empireStartLogo);
+});
